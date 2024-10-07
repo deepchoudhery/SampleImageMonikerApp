b3119bfe0d5fb20d9379413c653e79bf1dcd09bc
Deep Choudhery <decho@microsoft.com>
2024-10-06 20:33:48 -0700

    icon works now

---
+++ SampleApp/ImageMonikerConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using Microsoft.VisualStudio.Imaging.Interop;
+
+namespace SampleApp
+{
+    public class ImageMonikerConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ImageMoniker imageMoniker)
+            {
+                // Assume the image resource is in the correct path, use the resource name or logic to get ImageSource.
+                return new BitmapImage(new Uri("pack://application:,,,/Resources/SampleIcon.png"));
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SampleApp/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.Imaging.Interop;
+using System;
+using System.ComponentModel;
+
+namespace SampleApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ImageMoniker _sampleImageMoniker;
+
+        public MainViewModel()
+        {
+            _sampleImageMoniker = new ImageMoniker
+            {
+                Guid = new Guid("{D5B6C5B8-564B-400A-B81F-3C6BB85C0E22}"),
+                Id = 1
+            };
+        }
+
+        public ImageMoniker SampleImageMoniker
+        {
+            get { return _sampleImageMoniker; }
+            set
+            {
+                _sampleImageMoniker = value;
+                OnPropertyChanged(nameof(SampleImageMoniker));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
